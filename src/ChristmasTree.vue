a83d91320fa97b55afbcb8fa65147e54b7fa58e1
TomatoSoup <jonathantang0126@gmail.com>
2022-12-05 23:34:05 +0800

    feat: 🎸 add solution

--- src/ChristmasTree.vue
+++ src/ChristmasTree.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
+    <div v-if="((size - 1) > 0)">
+      <ChristmasTree :size="(size - 1)" />
+    </div>
     <div class="flex flex-row justify-center">
-      <!-- This div is styled for the tree sections, but it's not complete -->
-      <div class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
+      <div
+        v-for="number in size" :id="`${number}`"
+        class="relative rounded-full bg-green w-16 h-16 -m-2 flex justify-center items-center" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import ChristmasTree from './ChristmasTree.vue'
+
 withDefaults(
   defineProps<{
     size: number
